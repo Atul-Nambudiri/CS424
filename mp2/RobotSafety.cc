643b43b1b5248ae76b566257d19c65d623c7db05
Atul <Atul-Nambudiri@github.com>
2016-11-02 22:31:44 -0500

    First Commit

---
+++ mp2/RobotSafety.cc
@@ -0,0 +1,104 @@
+#include "RobotSafety.hh"
+
+void RobotSafety::stopAndPlaySong(pthread_mutex_t * stream_mutex, Create * robot, bool * stopped) {
+        //stop robot and play song
+        pthread_mutex_lock(stream_mutex);
+        robot->sendDriveCommand(0, Create::DRIVE_STRAIGHT);			
+        *stopped = true;
+        robot->sendPlaySongCommand(0);
+        sleep(1);
+        robot->sendPlaySongCommand(0);
+        pthread_mutex_unlock(stream_mutex);
+}
+
+void RobotSafety::startAgain(pthread_mutex_t * stream_mutex, Create * robot, int speed) {
+    pthread_mutex_lock(stream_mutex);
+    robot->sendDriveCommand(speed, Create::DRIVE_STRAIGHT);			
+	pthread_mutex_unlock(stream_mutex);
+}
+
+void * RobotSafety::overcurrent(void * args) {
+    RobotSafetyStruct * info = (RobotSafetyStruct *) args;
+    pthread_mutex_t * stream_mutex = info->stream_mutex;
+    Create * robot = info->robot;
+    int speed = info->speed;
+    bool * stopped = info->stopped;
+    pthread_cond_t * cv = info->cv;
+  
+    bool error = false;
+    cout << "Overcurrent Thread Starting" << endl;
+    while (1) {
+        //Check for overcurrent in either wheel
+        pthread_mutex_lock(stream_mutex);
+        bool left = robot->leftWheelOvercurrent();
+        bool right = robot->rightWheelOvercurrent();
+        pthread_mutex_unlock(stream_mutex);
+
+        if (left && right) {
+
+      	    cout << "overcurrent detected" << endl;
+	        error = true;
+
+	        this_thread::sleep_for(chrono::milliseconds(1000));
+
+	        //check again
+	        pthread_mutex_lock(stream_mutex);
+    	    bool left = robot->leftWheelOvercurrent();
+    	    bool right = robot->rightWheelOvercurrent();
+    	    pthread_mutex_unlock(stream_mutex);
+
+	        if (left && right) {
+	            error = true;
+	    
+	            cout << "overcurrent confirmed" << endl;
+
+	            //stop robot and play song
+	            stopAndPlaySong(stream_mutex, robot, stopped);
+	        }
+        } else if (error) {
+            *stopped = false;
+            error = false;
+            pthread_cond_broadcast(cv);
+	    }
+    }
+
+    return NULL;
+}
+
+void * RobotSafety::cliffWheelDrop(void * args) {
+    RobotSafetyStruct * info = (RobotSafetyStruct *) args;
+    pthread_mutex_t * stream_mutex = info->stream_mutex;
+    Create * robot = info->robot;
+    int speed = info->speed;
+    bool * stopped = info->stopped;
+    pthread_cond_t * cv = info->cv;
+
+    this_thread::sleep_for(chrono::milliseconds(1000)); // So it doens't beep in the beginning
+    
+    bool error = false;
+    cout << "Cliff Thread Starting" << endl;
+    cout << "Wheel Drop Thread Starting" << endl;
+    while (1) {
+	    pthread_mutex_lock(stream_mutex);
+	    short left = robot->cliffLeftSignal();
+	    short right = robot->cliffRightSignal();
+	    short frontLeft = robot->cliffFrontLeftSignal();
+	    short frontRight = robot->cliffFrontRightSignal();
+        bool leftWheel = robot->wheeldropLeft();
+        bool rightWheel = robot->wheeldropRight();
+        bool caster = robot->wheeldropCaster();
+	    pthread_mutex_unlock(stream_mutex);
+
+	    if (left < 100 || right < 100 || frontLeft < 100 || frontRight < 100 || leftWheel || rightWheel || caster) {
+		    cout << "cliff detected or wheel drop detected" << endl;
+            error = true;
+            stopAndPlaySong(stream_mutex, robot, stopped);
+	    } else if (error) {
+            *stopped = false;
+            error = false;
+            pthread_cond_broadcast(cv);
+        }
+
+    }
+    return NULL;
+}
---
+++ mp2/RobotSafety.hh
@@ -0,0 +1,34 @@
+#ifndef ROBOTSAFETY_HH
+#define ROBOTSAFETY_HH
+#include "irobot-create.hh"
+#include <mutex>
+#include <unistd.h>
+#include <pthread.h>
+#include <thread>
+
+using namespace iRobot;
+using namespace LibSerial;
+using namespace std;
+
+typedef struct RobotSafetyStruct {
+	int speed;
+	Create * robot;
+	pthread_mutex_t * stream_mutex;
+  bool * turning;
+  pthread_cond_t * cv;
+  bool * stopped;
+} RobotSafetyStruct;
+
+class RobotSafety {
+
+  RobotSafety() {}
+
+public:
+
+  static void stopAndPlaySong(pthread_mutex_t * stream_mutex, Create * robot, bool * stopped);
+  static void startAgain(pthread_mutex_t * stream_mutex, Create * robot, int speed);
+  static void * overcurrent(void * args);
+  static void * cliffWheelDrop(void * args);
+};
+
+#endif
